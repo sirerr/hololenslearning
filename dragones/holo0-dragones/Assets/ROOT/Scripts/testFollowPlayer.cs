0be729fa8ba2760ca5b335a41eea45de5cec2971
sirerr <sirerr@gmail.com>
2017-05-15 19:41:14 -0400

    first push

---
+++ dragones/holo0-dragones/Assets/ROOT/Scripts/testFollowPlayer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class testFollowPlayer : MonoBehaviour {
+
+	public Transform player;
+	public float speed =5f;
+	NavMeshAgent agent;
+	public bool move = false;
+	// Use this for initialization
+	void Start () {
+		agent = GetComponent<NavMeshAgent> ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		agent.destination = player.position;
+		if (move) {
+
+
+		}
+	}
+}
